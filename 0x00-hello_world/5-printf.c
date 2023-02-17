80c56e64e155c60f7f705b84f44147bff56e7139
kyalo460 <kyalo460@gmail.com>
2023-02-17 15:48:10 +0000

    Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -w 5-printf.c
+#include <stdio.h>
+
+/**
+  *main -Entry point
+  *printf - Sends output to standard output
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
