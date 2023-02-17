9d4df399a201101134e765b57e42e7b9520f391e
kyalo460 <kyalo460@gmail.com>
2023-02-17 16:10:46 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main -ENtry poin
+  *printf - Sends output to standard output
+  *sixeof - gives size of characters
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char = a;
+	int = b;
+	long int = c;
+	long long int =d;
+	float = f;
+
+	printf("Size of a char: %lu", sizeof(a));
+	printf("Size of a int: %lu", sizeof(b));
+	printf("Size of a long int: %lu", sizeof(c));
+	printf("Size of a long long int: %lu", sizeof(d));
+	printf("Size of a float: %lu", sizeof(f));
+
+	return (0);
+}
