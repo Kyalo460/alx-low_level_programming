4a28108d5772b1d401ba5c5e3a1ee252679b9a48
kyalo460 <kyalo460@gmail.com>
2023-02-17 15:23:01 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+  *main - Entry point
+  *puts - Sends output to standard output
+  *return - returns 0 if successful
+  */
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle  \n");
 	
 	return (0);
 }
