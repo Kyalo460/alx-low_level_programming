074065013c0fcba53b0a10a1607bc9bd701542a0
kyalo460 <kyalo460@gmail.com>
2023-02-17 16:52:52 +0000

    Task 7

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
-   * main - Entry point
-    *
-     * Return: Always 0 (Success)
-      */
+  *main - Entry point
+  *
+  *Return: Always 0 (Succuess)
+  */
 int main(void)
 {
-	    return (0);
+	return (0);
 }
