339631c47eeeddd15f7e401bd0effc8ea18ac7ec
kyalo460 <kyalo460@gmail.com>
2023-02-19 20:41:16 +0000

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
+  *main - Entry point
   *
-  *
-  *
+  *Return: Always 0 (Success)
   */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
