8ffbcb6424102724ad7bc9171433cadb2fdbd773
kyalo460 <kyalo460@gmail.com>
2023-02-20 17:10:47 +0000

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,10 +11,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-	
 		printf("%d", num);
 
-	putchar("\n");
+	printf("\n");
 	
 
 	return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+iinclude <stdio.h>
 
 /**
    * main - Prints all possible combinations of single-digit numbers.
@@ -13,7 +13,7 @@ int main(void)
 	{
 		putchar((num % 10) + '0');
 	
-		if (num == 0)
+		if (num == 9)
 		{
 			continue;
 		}
