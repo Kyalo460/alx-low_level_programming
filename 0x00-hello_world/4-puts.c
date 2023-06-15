29f9176eb79d2e8fd51716dd3d0d2aacb20b39ec
kyalo460 <kyalo460@gmail.com>
2023-06-15 13:21:07 +0000

    My first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+  *main - a program that prints a line of texts with puts function
+  *
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	char firstString[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(fisrtString);
+
+	return (0);
+}
