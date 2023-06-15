506947bb9e58fba23b2b6ba4429676721cf5efe5
kyalo460 <kyalo460@gmail.com>
2023-06-15 17:12:51 +0000

    Used a constant and write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
   */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, sizeof(message) -1);
+	char *line1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, line1, strlen(line1));
+
 	return (1);
 }
