fc765e10fc77b9ece41b133b1aef3263b83328d9
kyalo460 <kyalo460@gmail.com>
2023-06-15 13:30:14 +0000

    Printed a line using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio>
+/**
+  *main - prints a line using printf
+  *
+  *Return:0 (Code ended without error)
+  */
+int main(void)
+{
+	printf("%swith proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
