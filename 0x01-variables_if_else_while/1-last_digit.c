6b4a73a812c177b061a0f9b627f744776465a423
kyalo460 <kyalo460@gmail.com>
2023-06-16 13:41:03 +0000

    Added condition for positive integers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -31,16 +31,14 @@ int main(void)
 	/*Positive numbers*/
 	else if (n > 0)
 	{
-		lastdig = (n % 10);
+		lastdig = n % 10;
 
 		if ((lastdig < 6) && (lastdig != 0))
-		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
-		}
 		else if (lastdig > 5)
-		{
 			printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
-		}
+		else if (lastdig == 0)
+			printf("Last digit of %d is 0 and is 0", n);
 	}
 	return (0);
 }
