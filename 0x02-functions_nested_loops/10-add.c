eed54b5a7762e5597b418757a06cac8c6904f9c8
kyalo460 <kyalo460@gmail.com>
2023-06-20 18:19:54 +0000

    A program that adds two digits and returns the total

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *add - adds two integers
+  *@a: expects an int
+  *@b: expects an int
+  *Return: returns the sum
+  */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
