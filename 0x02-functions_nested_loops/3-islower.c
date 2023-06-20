4022f590498f48fc7e3dc7eabb4c04024fd35c0a
kyalo460 <kyalo460@gmail.com>
2023-06-20 14:28:27 +0000

    A program that return 1 if lowercase and 0 if not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *_islower - outputs 1 if character is lowercase and 0 if not
+  *Return: 0 if success
+  */
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+		return (1);
+	else
+		return (0);
+}
