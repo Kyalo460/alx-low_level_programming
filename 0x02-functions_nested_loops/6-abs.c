c3fba752c3e41be7f40f1c9abe98ff2949cee27d
kyalo460 <kyalo460@gmail.com>
2023-06-20 16:51:15 +0000

    A program that gives the absolute values for digits

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_abs - prints the absolute vaue of an integer
+  *@int: expects integer
+  *Return: 0 (success)
+  */
+int _abs(int digit)
+{
+
+	if (digit < 0)
+	{
+		return (-digit);
+	}
+	else
+	{
+		return (digit);
+	}
+}
