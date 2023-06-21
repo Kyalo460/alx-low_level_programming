f2cd0d4932c0e569e4e0fa7ddc796a9618a40344
kyalo460 <kyalo460@gmail.com>
2023-06-21 10:22:12 +0000

    Removed else statement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; num++)
 		{
@@ -41,6 +41,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
