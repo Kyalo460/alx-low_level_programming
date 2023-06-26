197d526ef666961a7c2042ed526eebe6b95db546
kyalo460 <kyalo460@gmail.com>
2023-06-26 14:17:31 +0000

    Tasks 4 and 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_rev - prints a string in reverse
+  *@s: expects string
+  */
+void print_rev(char *s)
+{
+	int length = 0;
+	int index;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
