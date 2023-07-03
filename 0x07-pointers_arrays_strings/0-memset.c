8ef34c7432d5098180fab079ce83b97f3c26e02d
kyalo460 <kyalo460@gmail.com>
2023-07-03 12:30:54 +0000

    Fixed betty error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,14 @@
 /**
   *_memset - fills the first n bytes of memory
   *@s: pointer to memory address to be filled
-  *@n: Number of bytes to be filles
   *@b: The byte to be added
+  *@n: Number of bytes to be filled
   *Return: returns the filled memory address
   */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-       	char *memory = s;
+	char *memory = s;
 	unsigned char value = b;
 
 	for (index = 0; index < n; index++)
