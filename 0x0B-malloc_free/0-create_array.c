62b67e4358104cbac062c947b29127ca5c0f0538
kyalo460 <kyalo460@gmail.com>
2023-07-10 15:12:18 +0000

    Changed code a bit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,18 +8,19 @@
   */
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
 	unsigned int i;
+	char *arr;
 
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(char) * size);
+	arr = malloc(sizeof(c) * size);
 
 	while (i <= size)
 	{
 		arr[i] = c;
 		i++;
 	}
+
 	return (arr);
 }
