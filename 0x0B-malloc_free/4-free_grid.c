3e6f474b4fdccfaf99c816f4b4198641b0f56fc7
kyalo460 <kyalo460@gmail.com>
2023-07-11 11:27:47 -0700

    A function that frees 2d array memory location

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *free_grid - Frees a 2d array
+ * *@grid: expects 2d array
+ * *@height: number of rows
+  */
+void free(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+
+	free(grid);
+}
