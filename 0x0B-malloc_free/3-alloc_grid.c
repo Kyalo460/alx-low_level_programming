ae73289721de4e61d1515dc9f77d5ff707fc98cf
kyalo460 <kyalo460@gmail.com>
2023-07-11 11:20:11 -0700

    Freed memory if null

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,26 +5,37 @@
  * *@width: expects number of rows
  * *@height: expects number of columns
  * *Return: returns the pointer to memory location if successful
- * */
+  */
 int **alloc_grid(int width, int height)
 {
 	int **array2d;
-	int i,j;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array2d = malloc(sizeof(int *) *height);
+	array2d = malloc(sizeof(int *) * height);
 
 	if (array2d == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		array2d[i] = malloc(sizeof(int) * width);
+
+		if (array2d[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(array2d[i]);
+
+			free(array2d);
+			return (NULL);
+		}
+	}
 
 	for (i = 0; i < height; i++)
 	{
-		for(j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 			array2d[i][j] = 0;
 	}
 
