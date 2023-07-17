255eaad28ded146ac498fdfc20a3ee2d2bf8877b
kyalo460 <kyalo460@gmail.com>
2023-07-17 10:11:33 +0000

    added braces arround 0

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,5 +4,5 @@
   *3-function_like_macro.h - defines a macro that provides the absolute value
   *@x: expects an number
   */
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-#endif
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+#endif /* FUNCTION_LIKE_MACRO_H */
