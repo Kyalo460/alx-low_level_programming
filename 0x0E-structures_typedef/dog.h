0bb6fcca73ce46289a0c1291490ba0f87bd34e8e
kyalo460 <kyalo460@gmail.com>
2023-07-17 14:17:47 +0000

    A structure that contains dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - contains details of a dog + owner
+  *@name: name of the dog
+  *@age: dog's age
+  *@owner: owner's name
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
