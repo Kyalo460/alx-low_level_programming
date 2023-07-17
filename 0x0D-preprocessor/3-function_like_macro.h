cd34d2ca5082fc4a6d2d8f69105affcddb735a67
kyalo460 <kyalo460@gmail.com>
2023-07-17 09:49:22 +0000

    Changed the name of first macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 /**
   *3-function_like_macro.h - defines a macro that provides the absolute value
   *@x: expects an number
