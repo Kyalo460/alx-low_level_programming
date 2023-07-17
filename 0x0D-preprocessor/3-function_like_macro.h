dc10506cf365784fea6c9ca8b131a7cd6a3b9a23
kyalo460 <kyalo460@gmail.com>
2023-07-17 09:46:48 +0000

    Changed the guard macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,8 @@
-#ifndef ABS
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+/**
+  *3-function_like_macro.h - defines a macro that provides the absolute value
+  *@x: expects an number
+  */
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
