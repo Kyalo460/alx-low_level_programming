fec4424edfc5e2937272c0fc41ba01baff1c76e3
kyalo460 <kyalo460@gmail.com>
2023-07-19 15:33:44 +0000

    Added a lengthier desc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
   *@name: expects an name
   *@f: pointer to function that prints a name
   *
-  *Description: receives an array then uses a pointer to function to 
+  *Description: receives an array then uses a pointer to function to
   *call a function that prints the name
   */
 void print_name(char *name, void (*f)(char *))
