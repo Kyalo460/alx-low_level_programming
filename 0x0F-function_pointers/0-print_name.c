e1778d5a96f6258c2f12b6d2ebf522d6a3f60ac5
kyalo460 <kyalo460@gmail.com>
2023-07-19 10:01:45 +0000

    Trying to pass checker

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
-  *print_name - contains pointer to function
+  *print_name - prints a name
   *@name: expects an name
-  *@f: pointer to function
+  *@f: pointer to function that prints a name
   */
 void print_name(char *name, void (*f)(char *))
 {
