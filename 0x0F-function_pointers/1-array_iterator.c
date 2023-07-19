79ca9c05be9ede492263b5afbc67f5b1466a7f2a
kyalo460 <kyalo460@gmail.com>
2023-07-19 15:05:27 +0000

    Added NULL checks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t index;
 	int num = 0;
 
+	if (action == NULL || array == NULL || size == 0)
+		return;
+
 	for (index = 0; index < size; index++)
 	{
 		num = array[index];
