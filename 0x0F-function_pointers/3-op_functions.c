db300178f83a702e493fefdb1f1faaa21d57b127
kyalo460 <kyalo460@gmail.com>
2023-07-21 07:23:19 +0000

    A calculator that takes arguments from terminal

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+  *op_add - adds two numbers
+  *@a: first number
+  *@b: second number
+  *
+  *Return: the sum of a and b
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the substraction of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The substraction of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The division of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the modulus of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The modulus of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
