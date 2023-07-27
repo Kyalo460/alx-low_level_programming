68e14c5fb208bf678fabb5a747675db0dbd566fc
kyalo460 <kyalo460@gmail.com>
2023-07-27 14:14:23 +0000

    added NULL check

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+		return (NULL);
+
+	if (duplicate == NULL)
 	{
 		free(new);
 		return (NULL);
