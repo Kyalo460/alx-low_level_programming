7f25cb05d0f02e0778000b253d9717239bbeecf7
kyalo460 <kyalo460@gmail.com>
2023-07-27 16:16:25 +0000

    Fixed betty errors

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 /**
+  *add_node_end - adds a node at end of list
+  *@head: address held at head
+  *@str: the string to be added
   *
+  *Return: returns the address of new memory or
+  *        NULL if it fails
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -12,24 +17,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	while(str[len])
+	while (str[len])
 		len++;
 
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
 
-	if(*head == NULL)
-		*head = new;
-
-	else
+	if (*head == NULL)
 	{
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-
-		temp->next = new;
+		*head = new;
+		return (new);
 	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
 
 	return (new);
 }
