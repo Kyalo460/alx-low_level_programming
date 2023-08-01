893c353a717150c74ced75fb7777bdd02cd75d95
kyalo460 <kyalo460@gmail.com>
2023-08-01 11:56:45 +0000

    freed new in function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count++;
 	}
 	if (current->next == NULL)
+	{
 		return (NULL);
+		free(new);
+	}
 
 	temp = current->next;
 	current->next = new;
