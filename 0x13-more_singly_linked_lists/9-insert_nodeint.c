985976f9a39a5b99ef686a6e209e6bd8e48ed5a9
kyalo460 <kyalo460@gmail.com>
2023-08-01 11:47:35 +0000

    Checked for NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,18 +16,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
+	if (*head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (idx == 0)
+	{
+		temp = current;
+		*head = new;
+		new->next = temp;
+		return (new);
+	}
+
 	while (count < (idx - 1))
 	{
 		current = current->next;
 		count++;
 	}
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (current->next == NULL)
 		return (NULL);
 
 	temp = current->next;
-	new->n = n;
 	current->next = new;
 	new->next = temp;
 
