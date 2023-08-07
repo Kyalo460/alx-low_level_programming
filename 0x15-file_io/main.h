00b9070343f6718b8534c226aa162f7fe584ac54
kyalo460 <kyalo460@gmail.com>
2023-08-07 16:09:21 +0000

    Pushing header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 /**
   *read_textfile - reads a textfile
   *@filename: pointer to the file
@@ -11,4 +12,5 @@
   *Return: actual number of characters read
   */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
