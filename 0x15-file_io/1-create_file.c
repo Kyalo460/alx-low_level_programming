def26f9e4b34485ec756b0ed050e25d2f5342a95
kyalo460 <kyalo460@gmail.com>
2023-08-07 16:55:36 +0000

    A function that makes a new file and writes to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  *create_file - creates a new file or appends to an existing one
+  *@filename: pointer to file name
+  *@text_content: content to be written in file
+  *
+  *Return: returns 1 (success) -1 (fail)
+  */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w;
+	int size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[size])
+			size++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_APPEND, 0600);
+	w = write(o, text_content, size);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
