0ed2948351ac58ec933ac77a2422dc2613e64c63
kyalo460 <kyalo460@gmail.com>
2023-09-14 16:50:53 +0000

    Can add node at the end of the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-	while (current->next && num < idx)
+	while (current && num < idx)
 	{
 		previous = current;
 		current = current->next;
