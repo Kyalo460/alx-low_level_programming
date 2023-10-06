cf03f4f96766d7f85a383ff9beb99d31c8eadf4b
kyalo460 <kyalo460@gmail.com>
2023-10-06 10:50:07 +0000

    Used strcmp to check condition

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index, size = 1024;
 	hash_node_t *current, *new_node = malloc(sizeof(hash_node_t));
 
-	if (key == "")
+	if (strcmp(key, "") == 0)
 		return (0);
 	if (new_node == NULL)
 		return (0);
