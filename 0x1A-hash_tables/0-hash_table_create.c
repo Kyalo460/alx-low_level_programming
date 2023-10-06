9a86e7e9e07157c01f4029b2386b651debfe3350
kyalo460 <kyalo460@gmail.com>
2023-10-06 12:23:19 +0000

    Returned null for size 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int x;
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table;
+
+	if (size == 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
 	new_table->size = size;
 
